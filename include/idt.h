90ca000787a4097b608885145324743e02596635
ShakedAvi <shakedaviram1@gmail.com>
2020-08-12 23:32:42 +0300

    Added IDT Files

---
+++ include/idt.h
@@ -0,0 +1,31 @@
+#ifndef IDT_H
+#define IDT_H
+
+#include "types.h"
+#include "util.h"
+
+#define KERNEL_CS 0x08
+
+typedef struct idt_gate_t
+{
+    uint16 low_offset;
+    uint16 sel;
+    uint8 always0;
+    uint8 flags;
+    uint16 high_offset;
+} __attribute__((packed)) idt_gate_t;
+
+typedef struct idt_register_t
+{
+    uint16 limit;
+    uint32 base;
+} __attribute__((packed)) idt_register_t;
+
+#define IDT_ENTRIES 256
+idt_gate_t idt[IDT_ENTRIES];
+idt_register_t idt_reg;
+
+void set_idt_gate(int n, uint32 handler);
+void set_idt();
+
+#endif
