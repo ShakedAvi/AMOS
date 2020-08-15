067040f196210930195abbdfac528d9b375138be
ShakedAvi <shakedaviram1@gmail.com>
2020-08-15 15:47:23 +0300

    Added IRQ Interrupt

--- include/idt.h
+++ include/idt.h
@@ -21,9 +21,20 @@ typedef struct idt_register_t
     uint32 base;
 } __attribute__((packed)) idt_register_t;
 
+typedef struct registers
+{
+   uint32 ds;                  // Data segment selector
+   uint32 edi, esi, ebp, esp, ebx, edx, ecx, eax; // Pushed by pusha.
+   uint32 int_no, err_code;    // Interrupt number and error code (if applicable)
+   uint32 eip, cs, eflags, useresp, ss; // Pushed by the processor automatically.
+} registers_t;
+
 #define IDT_ENTRIES 256
 idt_gate_t idt[IDT_ENTRIES];
 idt_register_t idt_reg;
+
+typedef void (*interrupt_handler_t)(registers_t *);
+interrupt_handler_t interrupt_handlers[IDT_ENTRIES];
 
 void set_idt_gate(int n, uint32 handler);
 void set_idt();
--- include/isr.c
+++ include/isr.c
@@ -1,8 +1,8 @@
 #include "isr.h"
 
-
 /* To print the message which defines every exception */
-string exception_messages[] = {
+string exception_messages[] =
+{
     "Division By Zero",
     "Debug",
     "Non Maskable Interrupt",
@@ -40,42 +40,83 @@ string exception_messages[] = {
     "Reserved"
 };
 
+void register_interrupt_handler(uint8 n, interrupt_handler_t handler)
+{
+    interrupt_handlers[n] = handler;
+}
+
 void isr_install()
 {
-    set_idt_gate(0, (uint32)isr0);
-    set_idt_gate(1, (uint32)isr1);
-    set_idt_gate(2, (uint32)isr2);
-    set_idt_gate(3, (uint32)isr3);
-    set_idt_gate(4, (uint32)isr4);
-    set_idt_gate(5, (uint32)isr5);
-    set_idt_gate(6, (uint32)isr6);
-    set_idt_gate(7, (uint32)isr7);
-    set_idt_gate(8, (uint32)isr8);
-    set_idt_gate(9, (uint32)isr9);
-    set_idt_gate(10, (uint32)isr10);
-    set_idt_gate(11, (uint32)isr11);
-    set_idt_gate(12, (uint32)isr12);
-    set_idt_gate(13, (uint32)isr13);
-    set_idt_gate(14, (uint32)isr14);
-    set_idt_gate(15, (uint32)isr15);
-    set_idt_gate(16, (uint32)isr16);
-    set_idt_gate(17, (uint32)isr17);
-    set_idt_gate(18, (uint32)isr18);
-    set_idt_gate(19, (uint32)isr19);
-    set_idt_gate(20, (uint32)isr20);
-    set_idt_gate(21, (uint32)isr21);
-    set_idt_gate(22, (uint32)isr22);
-    set_idt_gate(23, (uint32)isr23);
-    set_idt_gate(24, (uint32)isr24);
-    set_idt_gate(25, (uint32)isr25);
-    set_idt_gate(26, (uint32)isr26);
-    set_idt_gate(27, (uint32)isr27);
-    set_idt_gate(28, (uint32)isr28);
-    set_idt_gate(29, (uint32)isr29);
-    set_idt_gate(30, (uint32)isr30);
-    set_idt_gate(31, (uint32)isr31);
+  memory_set((uint32*)&interrupt_handlers, 0, sizeof (interrupt_handler_t) * IDT_ENTRIES);
 
-    set_idt(); // Load with ASM
+  idt_reg.limit = sizeof (idt_gate_t) * IDT_ENTRIES - 1;
+  idt_reg.base  = (uint32) &idt;
+
+  memory_set((uint32*)&idt, 0, sizeof (idt_gate_t) * IDT_ENTRIES - 1);
+
+  outportb(0x20, 0x11);
+  outportb(0xA0, 0x11);
+  outportb(0x21, 0x20);
+  outportb(0xA1, 0x28);
+  outportb(0x21, 0x04);
+  outportb(0xA1, 0x02);
+  outportb(0x21, 0x01);
+  outportb(0xA1, 0x01);
+  outportb(0x21, 0x0);
+  outportb(0xA1, 0x0);
+
+  set_idt_gate(0, (uint32)isr0);
+  set_idt_gate(1, (uint32)isr1);
+  set_idt_gate(2, (uint32)isr2);
+  set_idt_gate(3, (uint32)isr3);
+  set_idt_gate(4, (uint32)isr4);
+  set_idt_gate(5, (uint32)isr5);
+  set_idt_gate(6, (uint32)isr6);
+  set_idt_gate(7, (uint32)isr7);
+  set_idt_gate(8, (uint32)isr8);
+  set_idt_gate(9, (uint32)isr9);
+  set_idt_gate(10, (uint32)isr10);
+  set_idt_gate(11, (uint32)isr11);
+  set_idt_gate(12, (uint32)isr12);
+  set_idt_gate(13, (uint32)isr13);
+  set_idt_gate(14, (uint32)isr14);
+  set_idt_gate(15, (uint32)isr15);
+  set_idt_gate(16, (uint32)isr16);
+  set_idt_gate(17, (uint32)isr17);
+  set_idt_gate(18, (uint32)isr18);
+  set_idt_gate(19, (uint32)isr19);
+  set_idt_gate(20, (uint32)isr20);
+  set_idt_gate(21, (uint32)isr21);
+  set_idt_gate(22, (uint32)isr22);
+  set_idt_gate(23, (uint32)isr23);
+  set_idt_gate(24, (uint32)isr24);
+  set_idt_gate(25, (uint32)isr25);
+  set_idt_gate(26, (uint32)isr26);
+  set_idt_gate(27, (uint32)isr27);
+  set_idt_gate(28, (uint32)isr28);
+  set_idt_gate(29, (uint32)isr29);
+  set_idt_gate(30, (uint32)isr30);
+  set_idt_gate(31, (uint32)isr31);
+
+  set_idt_gate (32, (uint32)irq0);
+  set_idt_gate (33, (uint32)irq1);
+  set_idt_gate (34, (uint32)irq2);
+  set_idt_gate (35, (uint32)irq3);
+  set_idt_gate (36, (uint32)irq4);
+  set_idt_gate (37, (uint32)irq5);
+  set_idt_gate (38, (uint32)irq6);
+  set_idt_gate (39, (uint32)irq7);
+  set_idt_gate (40, (uint32)irq8);
+  set_idt_gate (41, (uint32)irq9);
+  set_idt_gate (42, (uint32)irq10);
+  set_idt_gate (43, (uint32)irq11);
+  set_idt_gate (44, (uint32)irq12);
+  set_idt_gate (45, (uint32)irq13);
+  set_idt_gate (46, (uint32)irq14);
+  set_idt_gate (47, (uint32)irq15);
+
+  set_idt(); // Load with ASM
+
 }
 
 /*Handlers*/
@@ -240,5 +281,18 @@ void isr31()
     asm("hlt");
 }
 
+void irq_handler(registers_t *regs)
+{
+    if (regs->int_no >= 40)
+    {
+        outportb(0xA0, 0x20);
+    }
+    outportb(0x20, 0x20);
+
+    if (interrupt_handlers[regs->int_no] != 0)
+    {
+      interrupt_handlers[regs->int_no] (regs);
+    }
+}
 
 /*End Handlers*/
