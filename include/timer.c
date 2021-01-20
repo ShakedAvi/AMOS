d5a98725361398fc2ccabad208e0a2d3cac15a9d
ShakedAvi <shakedaviram1@gmail.com>
2021-01-20 16:08:09 +0200

    Separated Timer Methods from Tasking Files

--- include/timer.c
+++ include/timer.c
@@ -1,1 +1,18 @@
 #include "timer.h"
+
+extern void timer_handler();
+
+void init_timer(uint32 frequency)
+{
+   register_interrupt_handler(32, (interrupt_handler_t)&timer_handler);
+
+   uint32 divisor = 1193182 / frequency;
+
+   outportb(0x43, 0x34);
+
+   uint8 l = (uint8)(divisor & 0xFF);
+   uint8 h = (uint8)((divisor>>8) & 0xFF);
+
+   outportb(0x40, l);
+   outportb(0x40, h);
+}
--- include/timer.h
+++ include/timer.h
@@ -1,6 +1,9 @@
 #ifndef TIMER_H
 #define TIMER_H
 
+#include "types.h"
+#include "isr.h"
 
+void init_timer(uint32 frequency);
 
 #endif
