7fc6cfbc384971aa6e56f489a7b8a622b05d7082
ShakedAvi <shakedaviram1@gmail.com>
2021-01-20 15:56:04 +0200

    Added Multiboot Files

--- kasm.asm
+++ kasm.asm
@@ -1,17 +1,43 @@
-bits 32
+MBOOT_PAGE_ALIGN    equ 1<<0    ; Load kernel and modules on a page boundary
+MBOOT_MEM_INFO      equ 1<<1    ; Provide your kernel with memory info
+MBOOT_HEADER_MAGIC  equ 0x1BADB002 ; Multiboot Magic value
+
+MBOOT_HEADER_FLAGS  equ MBOOT_PAGE_ALIGN | MBOOT_MEM_INFO
+MBOOT_CHECKSUM      equ -(MBOOT_HEADER_MAGIC + MBOOT_HEADER_FLAGS)
+
+;stack: resb 0x4000
+
+[BITS 32]
+
+[GLOBAL mboot]
+[EXTERN code]
+[EXTERN bss]
+[EXTERN end]
+
+section .__mbHeader
+
+mboot:
+    dd  MBOOT_HEADER_MAGIC
+
+    dd  MBOOT_HEADER_FLAGS
+    dd  MBOOT_CHECKSUM
+
+    dd  mboot
+    dd  code
+    dd  bss
+    dd  end
+    dd  start
 
 section .text
-  align 4
-  dd 0x1BADB002
-  dd 0x00
-  dd - (0x1BADB002+0x00)
 
-global _start
-extern kmain
+[GLOBAL start]
+[EXTERN kmain]
 
-_start:
-  push esp
+start:
+    ;mov esp, stack
+    push esp
+    push ebx
 
-  cli
-  call kmain
-  hlt
+    cli
+    call kmain
+    jmp $
