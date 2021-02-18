cf690a6e1431a2300de79c5bd4c2a885cff5d7d5
Kidsm <dror.malinsky@icloud.com>
2021-02-18 15:07:34 +0200

    Added PCI reader to search for AHCI Controller

---
+++ include/pci.c
@@ -0,0 +1,242 @@
+#include "pci.h"
+
+uint32 pci_size_map[100];
+pci_dev_t dev_zero = {0};
+//devices devices_array[]
+
+/*
+	Reads from PCI via port 0xCFC (PCI_CONFIG_DATA)
+	Input: PCI Device ( Saved as the Union pci_dev_t, contains data about the device like bus number, device number and function number)
+				 Field: Offset of data to read (Vendor Id, Class Id, Device Id ect.)
+  Output:
+				 Data from PCI
+*/
+uint32 pci_read(pci_dev_t dev, uint32 field) {
+	// Only most significant 6 bits of the field
+	dev.field_num = (field & 0xFC) >> 2;
+	dev.enable = 1;
+	outportl(PCI_CONFIG_ADDRESS, dev.bits);
+
+	// What size is this field supposed to be ?
+	uint32 size = pci_size_map[field];
+	if(size == 1)
+  {
+		// Get the first byte only, since it's in little endian, it's actually the 3rd byte
+		uint8 t = inportb(PCI_CONFIG_DATA + (field & 3));
+		return t;
+	}
+	else if(size == 2)
+  {
+		uint16 t = inports(PCI_CONFIG_DATA + (field & 2));
+		return t;
+	}
+	else if(size == 4)
+  {
+		// Read entire 4 bytes
+		uint32 t = inportl(PCI_CONFIG_DATA);
+		return t;
+	}
+	return 0xffff;
+}
+
+/*
+    Write pci field
+*/
+void pci_write(pci_dev_t dev, uint32 field, uint32 value)
+{
+	dev.field_num = (field & 0xFC) >> 2;
+	dev.enable = 1;
+	// Tell where we want to write
+	outportl(PCI_CONFIG_ADDRESS, dev.bits);
+	// Value to write
+	outportl(PCI_CONFIG_DATA, value);
+}
+
+/*
+    Get device type (i.e, is it a bridge, ide controller ? mouse controller? etc)
+*/
+uint32 get_device_type(pci_dev_t dev)
+{
+	uint32 t = pci_read(dev, PCI_CLASS) << 8;
+	return t | pci_read(dev, PCI_SUBCLASS);
+}
+
+/*
+		Get secondary bus from a PCI bridge device
+*/
+uint32 get_secondary_bus(pci_dev_t dev)
+{
+	return pci_read(dev, PCI_SECONDARY_BUS);
+}
+
+/*
+    Is current device an end point ? PCI_HEADER_TYPE 0 is end point
+*/
+uint32 pci_reach_end(pci_dev_t dev)
+{
+	uint32 t = pci_read(dev, PCI_HEADER_TYPE);
+	return !t;
+}
+
+/*
+	The following three functions are basically doing recursion, enumerating each and every device connected to pci
+	We start with the primary bus 0, which has 8 function, each of the function is actually a bus
+	Then, each bus can have 8 devices connected to it, each device can have 8 functions
+	When we gets to enumerate the function, check if the vendor id and device id match, if it does, we've found our device !
+*/
+
+/*
+    Scan function
+*/
+pci_dev_t pci_scan_function(uint32 bus, uint32 device, uint32 function)
+{
+	pci_dev_t dev = {0};
+	dev.bus_num = bus;
+	dev.device_num = device;
+	dev.function_num = function;
+
+	// If it's a PCI Bridge device, get the bus it's connected to and keep searching
+	if(get_device_type(dev) == PCI_TYPE_BRIDGE)
+  {
+		pci_scan_bus(get_secondary_bus(dev));
+	}
+	// If type matches, we've found the device, just return it
+	/*if(device_type == -1 || device_type == get_device_type(dev))
+  {
+		uint32 devid  = pci_read(dev, PCI_DEVICE_ID);
+		uint32 vendid = pci_read(dev, PCI_VENDOR_ID);
+		if(devid == device_id && vendor_id == vendid)
+		{
+      return dev;
+    }
+  }*/
+
+	uint32 device_id  = pci_read(dev, PCI_DEVICE_ID);
+	uint32 vendor_id = pci_read(dev, PCI_VENDOR_ID);
+	uint8 class_id = pci_read(dev, PCI_CLASS);
+	uint8 subclass_id = get_device_type(dev);
+	if (vendor_id != PCI_NONE)
+	{
+		print(" Subclass ID:");
+		char str3[8] = {0};
+		itoa(subclass_id, str3, 16);
+		print(" ");
+		print(str3);
+		print(" Class ID: ");
+		char str4[8] = {0};
+		itoa(class_id, str4, 16);
+		print(" ");
+		print(str4);
+		print("\n");
+	}
+	if(class_id == 0x01 && subclass_id == 0x06 && pci_read(dev, PCI_PROG_IF) == 0x01)
+	{
+		print("AHCI FOUND! \n");
+		return dev;
+	}
+	return dev_zero;
+}
+
+/*
+    Scan device
+*/
+pci_dev_t pci_scan_device(uint32 bus, uint32 device)
+{
+	pci_dev_t dev = {0};
+	dev.bus_num = bus;
+	dev.device_num = device;
+
+	if(pci_read(dev, PCI_VENDOR_ID) == PCI_NONE)
+	{
+		return dev_zero;
+	}
+
+	pci_dev_t t = pci_scan_function(bus, device, 0);
+	if(t.bits)
+	{
+		return t;
+	}
+
+	if(pci_reach_end(dev))
+	{
+		return dev_zero;
+	}
+
+	for(int function = 1; function < FUNCTION_PER_DEVICE; function++)
+  {
+		if(pci_read(dev,PCI_VENDOR_ID) != PCI_NONE)
+    {
+			t = pci_scan_function(bus, device, function);
+			if(t.bits)
+			{
+				return t;
+			}
+		}
+	}
+	return dev_zero;
+}
+
+/*
+    Scan bus
+*/
+pci_dev_t pci_scan_bus(uint32 bus)
+{
+	for(int device = 0; device < DEVICE_PER_BUS; device++)
+  {
+		pci_dev_t t = pci_scan_device(bus, device);
+		if(t.bits)
+		{
+			return t;
+		}
+	}
+	return dev_zero;
+}
+
+/*
+	Returns the data from the ABAR (AHCI Base Memory Register), the abar contains all AHCI registers and memories.
+	Input:
+		None
+	Output:
+		HBA_MEM* abar: a struct containing the data from the ABAR 
+*/
+HBA_MEM* get_abar()
+{
+	pci_dev_t ahci_driver = pci_scan_bus(0);
+	print("Reading ABAR...\n");
+	uint32 bar5 = pci_read(ahci_driver, PCI_BAR5) & 0xFFFFFFF0;
+	/*if(bar5 == HBA_MEM_BASE_ADDRESS)
+	{
+		print("Epic");
+	}*/
+	HBA_MEM* abar = (HBA_MEM*)HBA_MEM_BASE_ADDRESS;
+	return abar;
+}
+
+/*
+    PCI Init, filling size for each field in config space
+		Input: None
+		Output: None
+*/
+void pci_init()
+{
+	// Init size map
+	pci_size_map[PCI_VENDOR_ID] =	2;
+	pci_size_map[PCI_DEVICE_ID] =	2;
+	pci_size_map[PCI_COMMAND]	=	2;
+	pci_size_map[PCI_STATUS]	=	2;
+	pci_size_map[PCI_SUBCLASS]	=	1;
+	pci_size_map[PCI_CLASS]		=	1;
+	pci_size_map[PCI_CACHE_LINE_SIZE]	= 1;
+	pci_size_map[PCI_LATENCY_TIMER]		= 1;
+	pci_size_map[PCI_HEADER_TYPE] = 1;
+	pci_size_map[PCI_BIST] = 1;
+	pci_size_map[PCI_BAR0] = 4;
+	pci_size_map[PCI_BAR1] = 4;
+	pci_size_map[PCI_BAR2] = 4;
+	pci_size_map[PCI_BAR3] = 4;
+	pci_size_map[PCI_BAR4] = 4;
+	pci_size_map[PCI_BAR5] = 4;
+	pci_size_map[PCI_INTERRUPT_LINE]	= 1;
+	pci_size_map[PCI_SECONDARY_BUS]		= 1;
+
+}
---
+++ include/pci.h
@@ -0,0 +1,85 @@
+#ifndef PCI_H
+#define PCI_H
+
+#include "system.h"
+#include "util.h"
+#include "string.h"
+#include "screen.h"
+#include "ahci.h"
+
+typedef union pci_dev
+{
+    uint32 bits;
+    struct {
+        uint32 always_zero    : 2;
+        uint32 field_num      : 6;
+        uint32 function_num   : 3;
+        uint32 device_num     : 5;
+        uint32 bus_num        : 8;
+        uint32 reserved       : 7;
+        uint32 enable         : 1;
+    };
+} pci_dev_t;
+
+/*typedef struct devices
+{
+  uint32 device_id;
+  uint32 vendor_id;
+  uint8 class_id;
+  uint8 subclass_id;
+}devices*/;
+
+// Ports
+#define PCI_CONFIG_ADDRESS  0xCF8
+#define PCI_CONFIG_DATA     0xCFC
+
+// Config Address Register
+
+// Offset
+#define PCI_VENDOR_ID            0x00
+#define PCI_DEVICE_ID            0x02
+#define PCI_COMMAND              0x04
+#define PCI_STATUS               0x06
+#define PCI_REVISION_ID          0x08
+#define PCI_PROG_IF              0x09
+#define PCI_SUBCLASS             0x0a
+#define PCI_CLASS                0x0b
+#define PCI_CACHE_LINE_SIZE      0x0c
+#define PCI_LATENCY_TIMER        0x0d
+#define PCI_HEADER_TYPE          0x0e
+#define PCI_BIST                 0x0f
+#define PCI_BAR0                 0x10
+#define PCI_BAR1                 0x14
+#define PCI_BAR2                 0x18
+#define PCI_BAR3                 0x1C
+#define PCI_BAR4                 0x20
+#define PCI_BAR5                 0x24
+#define PCI_INTERRUPT_LINE       0x3C
+#define PCI_SECONDARY_BUS        0x09
+
+// Device type
+#define PCI_HEADER_TYPE_DEVICE  0
+#define PCI_HEADER_TYPE_BRIDGE  1
+#define PCI_HEADER_TYPE_CARDBUS 2
+#define PCI_TYPE_BRIDGE 0x0604
+#define PCI_TYPE_SATA   0x0106
+#define PCI_NONE 0xFFFF
+
+//Address of BAR5, Used to get the data we need from the AHCI driver.
+#define HBA_MEM_BASE_ADDRESS 0xfebd5000
+#define DEVICE_PER_BUS           32
+#define FUNCTION_PER_DEVICE      32
+
+uint32 pci_read(pci_dev_t dev, uint32 field);
+void pci_write(pci_dev_t dev, uint32 field, uint32 value);
+uint32 get_device_type(pci_dev_t dev);
+uint32 get_secondary_bus(pci_dev_t dev);
+uint32 pci_reach_end(pci_dev_t dev);
+pci_dev_t pci_scan_function(uint32 bus, uint32 device, uint32 function);
+pci_dev_t pci_scan_device(uint32 bus, uint32 device);
+pci_dev_t pci_scan_bus(uint32 bus);
+pci_dev_t pci_get_device(uint16 vendor_id, uint16 device_id, int device_type);
+void pci_init();
+HBA_MEM* get_abar();
+
+#endif
