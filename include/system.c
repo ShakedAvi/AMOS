0dc2d88bf9578b1f9f1d88eca9b66624fd409d0c
Kidsm <dror.malinsky@icloud.com>
2021-02-18 15:06:53 +0200

    Added port reading functions

--- include/system.c
+++ include/system.c
@@ -19,3 +19,27 @@ void outportb(uint16 port, uint8 data)
 {
   __asm__ __volatile__ ("outb %1, %0" : : "dN" (port), "a" (data));
 }
+
+uint16 inports(uint16 port)
+{
+  uint16 rv;
+  __asm__ __volatile__ ("inw %1, %0" : "=a" (rv) : "dN" (port));
+  return rv;
+}
+
+void outports(uint16 port, uint16 data)
+{
+  __asm__ __volatile__ ("outw %1, %0" : : "dN" (port), "a" (data)); //Might not work cuz assembly is fun
+}
+
+uint16 inportl(uint16 port)
+{
+  uint32 rv;
+  __asm__ __volatile__ ("inl %1, %0" : "=a" (rv) : "dN" (port));
+  return rv;
+}
+
+void outportl(uint16 port, uint32 data)
+{
+  __asm__ __volatile__ ("outl %1, %0" : : "dN" (port), "a" (data));
+}
--- include/system.h
+++ include/system.h
@@ -6,5 +6,9 @@
 int myRand();
 uint8 inportb(uint16 port);
 void outportb(uint16 port, uint8 data);
+uint16 inports(uint16 port);
+void outports(uint16 port, uint16 data);
+uint16 inportl(uint16 port);
+void outportl(uint16 port, uint32 data);
 
 #endif
